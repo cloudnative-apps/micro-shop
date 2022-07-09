ecc5dee83c5855c4eb603dc7d92f2974078e6d67
Sharad <sharadit2011@gmail.com>
2022-07-09 09:22:25 +0530

    project setup

---
+++ src/WebApps/Shop.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Shop.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ src/WebApps/Shop.WebStatus/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Shop.WebStatus.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
